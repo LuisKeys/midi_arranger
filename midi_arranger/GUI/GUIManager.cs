959754f2f195fb1c65b79f8755240de090b57d8a
Luis Luis <luis.paradela@gmail.com>
2020-09-23 19:29:56 -0300

    GUI wip

---
+++ midi_arranger/GUI/GUIManager.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace midi_arranger
+{
+    public class GUIManager
+    {
+        const int STL_NUM_COLUMNS = 4;
+        const int STL_NUM_ROWS = 4;
+        
+        const int VAR_NUM_COLUMNS = 8;
+        const int VAR_NUM_ROWS = 2;
+        const double VAR_MATRIX_AREA = 0.5;
+        const double VAR_BUTTON_PADDING = 0.05;
+        const string VAR_BUTTON_NAME_PREFIX = "var_";
+        const double STL_MATRIX_AREA = 0.5;
+        const string STL_BUTTON_NAME_PREFIX = "style_";
+        const double STL_BUTTON_PADDING = 0.02;
+        const double BLOCKS_PADDING = 0.04;
+
+        static Color EMPTY_VAR_COLOR = Color.FromArgb(10, 20, 20);
+        static Color FONT_COLOR  = Color.White;
+        static Color STL_COLOR = Color.FromArgb(8, 15, 15);
+
+        static bool guiInialized = false;
+
+        public static void Resize(MainForm mainForm)
+        {
+            if (!guiInialized) 
+            {
+                initGUI(mainForm);
+            }
+            else 
+            {
+                resizeGUI(mainForm);
+            }
+        }
+        
+        private static Point getVarButtonSize(MainForm mainForm, double padding) 
+        {
+            int width = Convert.ToInt32(Convert.ToDouble(mainForm.Width) / VAR_NUM_COLUMNS);
+            int height = Convert.ToInt32(Convert.ToDouble(mainForm.Height) * VAR_MATRIX_AREA / VAR_NUM_ROWS);
+            double net_size = (1.0 - padding * 2.0);
+            int witdh_with_padding = Convert.ToInt32(Convert.ToDouble(width) * net_size);
+            int height_with_padding = Convert.ToInt32(Convert.ToDouble(height) * net_size);
+            return new Point(witdh_with_padding, height_with_padding);
+        }
+
+        private static Point getVarButtonPosition(MainForm mainForm, int buttonIndex)
+        {
+            Point buttonSize = getVarButtonSize(mainForm, 0.0);
+            int buttonsCount = Convert.ToInt32(VAR_NUM_COLUMNS * VAR_NUM_ROWS);
+            int col = buttonIndex % VAR_NUM_COLUMNS;
+            int row = (buttonIndex - col) / VAR_NUM_COLUMNS;
+            int x = col * buttonSize.X;
+            int y = row * buttonSize.Y;
+
+            x += Convert.ToInt32(Convert.ToDouble(buttonSize.X) * VAR_BUTTON_PADDING);
+            y += Convert.ToInt32(Convert.ToDouble(buttonSize.Y) * VAR_BUTTON_PADDING);
+
+            return new Point(x, y);
+        }
+
+        private static Point getStyleButtonSize(MainForm mainForm, double padding)
+        {
+            int width = Convert.ToInt32(Convert.ToDouble(mainForm.Width) / STL_NUM_COLUMNS);
+            int height = Convert.ToInt32(Convert.ToDouble(mainForm.Height) * STL_MATRIX_AREA / STL_NUM_ROWS);
+            height -= Convert.ToInt32(Convert.ToDouble(mainForm.Height) * BLOCKS_PADDING / STL_NUM_ROWS);
+            double net_size_x = (1.0 - padding * 2.0);
+            double net_size_y = (1.0 - padding * 10.0);
+            int witdh_with_padding = Convert.ToInt32(Convert.ToDouble(width) * net_size_x);
+            int height_with_padding = Convert.ToInt32(Convert.ToDouble(height) * net_size_y);
+            return new Point(witdh_with_padding, height_with_padding);
+        }
+
+        private static Point getStyleButtonPosition(MainForm mainForm, int buttonIndex)
+        {
+            Point buttonSize = getStyleButtonSize(mainForm, 0.0);
+            int buttonsCount = Convert.ToInt32(STL_NUM_COLUMNS * STL_NUM_ROWS);
+            int col = buttonIndex % STL_NUM_COLUMNS;
+            int row = (buttonIndex - col) / STL_NUM_COLUMNS;
+            int x = col * buttonSize.X;
+            int y = row * buttonSize.Y;
+            int style_buttons_offset = Convert.ToInt32(Convert.ToDouble(mainForm.Height) * VAR_MATRIX_AREA);
+            int blocks_padding = Convert.ToInt32(Convert.ToDouble(mainForm.Height) * BLOCKS_PADDING);
+
+            x += Convert.ToInt32(Convert.ToDouble(buttonSize.X) * STL_BUTTON_PADDING);
+            y += Convert.ToInt32(Convert.ToDouble(buttonSize.Y) * STL_BUTTON_PADDING);
+
+            y += style_buttons_offset;
+            y += blocks_padding;
+
+            return new Point(x, y);
+        }
+
+        private static void initGUI(MainForm mainForm) 
+        {
+            guiInialized = true;
+            Point buttonSize = getVarButtonSize(mainForm, VAR_BUTTON_PADDING);
+
+            for (int i = 0; i < VAR_NUM_COLUMNS * VAR_NUM_ROWS; ++i) 
+            {
+                Point buttonPosition = getVarButtonPosition(mainForm, i);
+                Button varButton = new System.Windows.Forms.Button();
+                varButton.Font = new System.Drawing.Font("Courier New Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                varButton.Location = new System.Drawing.Point(buttonPosition.X, buttonPosition.Y);
+                varButton.Name = VAR_BUTTON_NAME_PREFIX + i.ToString();
+                varButton.Size = new System.Drawing.Size(buttonSize.X, buttonSize.Y);
+                varButton.TabIndex = i;
+                varButton.Text = (i + 1).ToString();
+                varButton.ForeColor = FONT_COLOR;
+                varButton.BackColor = EMPTY_VAR_COLOR;
+                varButton.FlatStyle = FlatStyle.Flat;
+                mainForm.Controls.Add(varButton);
+            }
+            
+            buttonSize = getStyleButtonSize(mainForm, VAR_BUTTON_PADDING);
+
+            for (int i = 0; i < STL_NUM_COLUMNS * STL_NUM_ROWS; ++i)
+            {
+                Point buttonPosition = getStyleButtonPosition(mainForm, i);
+                Button styleButton = new System.Windows.Forms.Button();
+                styleButton.Font = new System.Drawing.Font("Courier New Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                styleButton.Location = new System.Drawing.Point(buttonPosition.X, buttonPosition.Y);
+                styleButton.Name = STL_BUTTON_NAME_PREFIX + i.ToString();
+                styleButton.Size = new System.Drawing.Size(buttonSize.X, buttonSize.Y);
+                styleButton.TabIndex = i;
+                styleButton.Text = (i + 1).ToString();
+                styleButton.ForeColor = FONT_COLOR;
+                styleButton.BackColor = STL_COLOR;
+                styleButton.FlatStyle = FlatStyle.Flat;
+                mainForm.Controls.Add(styleButton);
+            }
+        }
+
+        private static void resizeGUI(MainForm mainForm)
+        {
+            guiInialized = true;
+            Point buttonSize = getVarButtonSize(mainForm, VAR_BUTTON_PADDING);
+
+            for (int i = 0; i < VAR_NUM_COLUMNS * VAR_NUM_ROWS; ++i)
+            {
+                Point buttonPosition = getVarButtonPosition(mainForm, i);
+                System.Windows.Forms.Button varButton = (System.Windows.Forms.Button)mainForm.Controls.Find(VAR_BUTTON_NAME_PREFIX + i.ToString(), false)[0];
+                varButton.Location = new System.Drawing.Point(buttonPosition.X, buttonPosition.Y);
+                varButton.Size = new System.Drawing.Size(buttonSize.X, buttonSize.Y);
+            }
+
+            buttonSize = getStyleButtonSize(mainForm, STL_BUTTON_PADDING);
+
+            for (int i = 0; i < STL_NUM_COLUMNS * STL_NUM_ROWS; ++i)
+            {
+                Point buttonPosition = getStyleButtonPosition(mainForm, i);
+                System.Windows.Forms.Button styleButton = (System.Windows.Forms.Button)mainForm.Controls.Find(STL_BUTTON_NAME_PREFIX + i.ToString(), false)[0];
+                styleButton.Location = new System.Drawing.Point(buttonPosition.X, buttonPosition.Y);
+                styleButton.Size = new System.Drawing.Size(buttonSize.X, buttonSize.Y);
+            }
+
+        }
+    }
+}
---
+++ midi_arranger/MainForm.Designer.cs
@@ -0,0 +1,54 @@
+﻿
+namespace midi_arranger
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+    }
+}
+
